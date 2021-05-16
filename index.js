be32d9068eccf9c1e77fa1f2669c78b870a78fb6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-05-16 23:15:33 +0800

    多个渲染进程向主进程发送同样的消息，消息发送完成之后，需要主进程响应消息给发送者，也就是发送消息给对应的渲染进程

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.on('ready', function () {
         win.webContents.send('msg_main2render', param1, param2)
     })
 
+    ipcMain.on('msg_render3main', (event, param1, param2) => {
+        console.log('主进程接收到消息1', param1)
+        console.log('主进程接收到消息2', param2)
+        console.log('主进程接收到发送者', event.sender)
+        // 主进程接收消息的事件的 event.sender 代表发送消息的渲染进程的 webContents，所以可以通过使用这个对象来给对应的窗口发送消息
+        event.sender.send('msg_main3render', param1, param2)
+    })
+
 })
 
 app.on('window-all-closed', function () {
