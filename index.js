7d0941af7b428ad7aaec883f47f37744a5a6e195
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-05-21 19:45:35 +0800

    恢复误删代码

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.on('ready', function () {
     })
 
     ipcMain.on('msg_render2main', (event, param1, param2) => {
+        console.log('主进程接收到消息1', param1)
+        console.log('主进程接收到消息2', param2)
+        console.log('主进程接收到发送者', event.sender)
+        win.webContents.send('msg_main2render', param1, param2)
+    })
+
+    ipcMain.on('msg_render3main', (event, param1, param2) => {
+        console.log('主进程接收到消息1', param1)
+        console.log('主进程接收到消息2', param2)
+        console.log('主进程接收到发送者', event.sender)
+        // 主进程接收消息的事件的 event.sender 代表发送消息的渲染进程的 webContents，所以可以通过使用这个对象来给对应的窗口发送消息
+        event.sender.send('msg_main3render', param1, param2)
+        // 原理同上
+        // event.reply('msg_main3render', param1, param2)
         console.log(param1)
         console.log(param2)
         console.log(event.sender)
